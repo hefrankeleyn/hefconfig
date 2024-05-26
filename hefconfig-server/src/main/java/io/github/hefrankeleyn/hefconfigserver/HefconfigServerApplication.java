ff40c043c098f08ed81dbe8c8972974b9d441a5c
Li Fei <hefrankeleyn@gamil.com>
2024-05-26 14:35:44 +0800

    Step 1.0: 初始化hefconfig-server

---
+++ hefconfig-server/src/main/java/io/github/hefrankeleyn/hefconfigserver/HefconfigServerApplication.java
@@ -0,0 +1,13 @@
+package io.github.hefrankeleyn.hefconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HefconfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HefconfigServerApplication.class, args);
+    }
+
+}
---
+++ hefconfig-server/src/test/java/io/github/hefrankeleyn/hefconfigserver/HefconfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.hefrankeleyn.hefconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HefconfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
