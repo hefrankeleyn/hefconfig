49c41fa3f70abc3e72ed170238ba3b5b77841373
Li Fei <hefrankeleyn@gamil.com>
2024-06-04 13:07:48 +0800

    Step 4.3 优化：只更新更新的配置

--- hefconfig-client/src/main/java/io/github/hefrankeleyn/hefconfig/client/service/impl/HefConfigServiceImpl.java
+++ hefconfig-client/src/main/java/io/github/hefrankeleyn/hefconfig/client/service/impl/HefConfigServiceImpl.java
@@ -5,8 +5,10 @@ import io.github.hefrankeleyn.hefconfig.client.service.HefConfigService;
 import org.springframework.cloud.context.environment.EnvironmentChangeEvent;
 import org.springframework.context.ApplicationContext;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * @Date 2024/5/28
@@ -34,9 +36,40 @@ public class HefConfigServiceImpl implements HefConfigService {
 
     @Override
     public void onChange(ChangeEvent changeEvent) {
+        Set<String> diffKeys = getUpdateKeys(this.config, changeEvent.getConfigs());
+        if (diffKeys.isEmpty()) {
+            return;
+        }
         this.config = changeEvent.getConfigs();
         if (Objects.nonNull(this.config) && !this.config.isEmpty()) {
-            applicationContext.publishEvent(new EnvironmentChangeEvent(this.config.keySet()));
+            System.out.println("====> [hefconfig] fire update config keys:" + diffKeys);
+            applicationContext.publishEvent(new EnvironmentChangeEvent(diffKeys));
         }
+    }
+
+    private Set<String> getUpdateKeys(Map<String, String> oldConfig, Map<String, String> newConfigs) {
+        Set<String> diffKeys = new HashSet<>();
+        if (mapIsEmpty(oldConfig) && mapIsEmpty(newConfigs)) {
+            return diffKeys;
+        } else if (mapIsEmpty(oldConfig) && !mapIsEmpty(newConfigs)) {
+            return newConfigs.keySet();
+        } else if (!mapIsEmpty(oldConfig) && mapIsEmpty(newConfigs)) {
+            return oldConfig.keySet();
+        }
+        Set<String> allKeys = new HashSet<>();
+        allKeys.addAll(oldConfig.keySet());
+        allKeys.addAll(newConfigs.keySet());
+        for (String oneKey : allKeys) {
+            if (!(oldConfig.containsKey(oneKey) && newConfigs.containsKey(oneKey))) {
+                diffKeys.add(oneKey);
+            } else if (!oldConfig.get(oneKey).equals(newConfigs.get(oneKey))) {
+                diffKeys.add(oneKey);
+            }
+        }
+        return diffKeys;
+    }
+
+    private boolean mapIsEmpty(Map<String, String> map) {
+        return map == null || map.isEmpty();
     }
 }
